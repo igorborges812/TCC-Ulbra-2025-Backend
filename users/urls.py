07f85d6df24f1f1004a689b0f6d0bc014c971767
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-09-29 23:55:07 -0300

    Refactor settings.py and urls.py to include API authentication and documentation endpoints

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,6 @@ from .views import (CustomTokenObtainPairView, CustomTokenRefreshView,
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('logout/', TokenVerifyView.as_view(), name='token_verify'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('edit/', UserUpdateView.as_view(), name='user_edit'),
     path('deactivate/', DeactivateUserView.as_view(), name='user_deactivate'),
