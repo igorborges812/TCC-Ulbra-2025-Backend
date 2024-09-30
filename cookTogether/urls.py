6ce097506d65198aee12572556e88cb4504184c6
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-09-29 23:23:21 -0300

    Refactor URL configuration to include API endpoints for users and recipes

--- cookTogether/urls.py
+++ cookTogether/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
-    path('api/recipes/', include('recipes.urls')),
+    # path('api/recipes/', include('recipes.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
-# Create your views here.
+from .serializers import (CustomTokenObtainPairSerializer,
+                          UserRegistrationSerializer, UserUpdateSerializer)
+
+User = get_user_model()
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserRegistrationSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
+
+class UserUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user  # Usuário autenticado
+
+class DeactivateUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        user.deactivate_account()
+        return Response({"detail": "Conta desativada com sucesso."}, status=status.HTTP_204_NO_CONTENT)
