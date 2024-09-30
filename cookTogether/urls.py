e558c60a17218e91ba163a26f66405b845bbe3d5
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2024-09-29 23:31:14 -0300

    Add drf_yasg to INSTALLED_APPS and include API documentation endpoints

--- cookTogether/urls.py
+++ cookTogether/urls.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="CookTogether API",
+      default_version='v1',
+      description="Documentação da API para o projeto de receitas",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="suporte@cooktogether.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     # path('api/recipes/', include('recipes.urls')),
+    
+    # Rotas para documentação Swagger e Redoc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
